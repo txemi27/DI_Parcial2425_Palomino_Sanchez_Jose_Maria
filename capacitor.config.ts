733c88288ed8f60657445ad6c85ce1eb9c5fb994
di-birt <mardanza@birt.eus>
2025-01-08 13:26:48 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI_PlantillaExamenParcial2425',
+  webDir: 'www'
+};
+
+export default config;
