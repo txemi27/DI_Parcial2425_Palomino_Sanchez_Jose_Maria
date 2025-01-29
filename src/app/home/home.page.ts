fcfdfeaed2736f659f0cfa3dd581ce6e5f842c17
Jose Maria Palomino <jmpalomino@airlan.es>
2025-01-29 19:35:22 +0100

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,15 +9,23 @@ import { IPregunta } from './../interfaces/interfaces';
   standalone: false,
 })
 export class HomePage {
+logotipos: string[] = ["assets/icon/volkswagen.jpg","assets/icon/apple.jpg","assets/icon/Shell.jpg","assets/icon/starbucks.jpg",];
+preguntasService = this.cuestionarServicio.preguntas;
 
   //Importar servicio
-  constructor() {}
+  constructor(private cuestionarServicio: CuestionarioService) {}
 
   //Crear método para gestionar el onclick de RESPONDER
   //Recibirá un IPregunta y llamará al servicio para realizar las operaciones necesarias.
+  responder(){
+    this.cuestionarServicio.presentAlert();
+  }
 
   //Crear método para gestionar el onclick de Guardar
   //No recibe parámetros y llamará al servicio para realizar las operaciones necesarias.
+  guardar(){
+    
+  }
 
-
+  marcas: string[] = [];
 }
--- src/app/servicios/cuestionario.service.ts
+++ src/app/servicios/cuestionario.service.ts
@@ -5,15 +5,17 @@ import { Observable } from 'rxjs';
 import { GestionStorageService } from './gestion-storage.service';
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class CuestionarioService {
   // Array para almacenar todas las preguntas del json. Recordad inicializar el array para evitar problemas
-
+preguntas: IPregunta[] = [];
   //Añadir los componentes y servicios que se necesitan
-  constructor() {
+  constructor(private alertController: AlertController, private leerFichero: HttpClient, private gestionAlmacenamiento: GestionStorageService) {
     //Cargar los datos
+    this.getDatosFichero();
   }
 
   // Método que devolverá un array de IPregunta, es decir, todas las preguntas del cuestionario en un array
@@ -24,15 +26,50 @@ export class CuestionarioService {
 
 
   // Lee los datos de un fichero y los almacena en un array
+getDatosFichero(){
+  let datosFichero: Observable<IPregunta[]>;
+datosFichero = this.leerFichero.get<IPregunta[]>("assets/datos/datos.json");
+ datosFichero.subscribe(datos =>{
+  console.log(datos);
+  this.preguntas.push(...datos);
+ })
 
+}
 
   // Abre una alerta con el enunciado de la pregunta y comprueba la respuesta
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: '¿De que marca es el logotipo?',
+      inputs: [{
+        name: 'enviar',
+        placeholder: 'Atención a la ortografia',
+        type: 'text',
+      }
+        
+      ],
+      buttons: [ 
+      {
+        text: 'Enviar',
+        role: 'confirm',
+        handler: (data) => {
+          console.log(data);
+          this.comprobarRespuesta(data)
+        },
+      },],
+    });
+
+    await alert.present();
+  }
   // 1- En función de si es correcta o no, actualiza el estado de acierto.
   // 2- Si no se acierta:
   // 2.1- Restará el valor de los intentos
   // 2.2- Guardará el valor añadido en el array respuestasIncorrectas
+comprobarRespuesta(data: string){
 
+}
 
   // Almacenar el array de preguntas en Storage
+almacenarPreguntasStorage(data: string){
 
+}
 }
